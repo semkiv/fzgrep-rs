770af5c4bcc4dfb0238fc7d275b5e823e2a4cc72
Andrii Semkiv <semkiv@gmail.com>
2023-10-25 22:45:12 +0200

    Fuzzy matching and scoring are now offloaded onto a separate crate.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # fzgrep-rs
-Fuzzy grep implemented in Rust
+
+Fuzzy grep implemented in Rust.
+Uses [vscode-fuzzy-score-rs](https://github.com/semkiv/vs-code-fuzzy-score-rs) as a fuzzy matching and scoring library.
+⚠️ WARNING: at this point this is still WIP.
--- src/lib.rs
+++ src/lib.rs
@@ -1,62 +1,72 @@
-mod fuzzy_score;
-
-use fuzzy_score::FuzzyMatch;
-
-use clap::{Arg, ArgMatches, Command};
 use colored::Colorize;
-
+use details::Config;
 use std::env;
+use vscode_fuzzy_score_rs;
 
 /// This function handles all the application logic. The `main` function is merely a `run` call.
+/// The query and the target are taken from positional command line argument,
+/// the first and the second ones respectively.
 pub fn run() {
     let config = Config::new(env::args());
-    let fuzzy_match = FuzzyMatch::new(&config.pattern, &config.file);
-    let score = fuzzy_match.score();
-    let mut s = String::new();
-    for item in fuzzy_match.matches().iter() {
-        if item.is_match {
-            s.push_str(&item.character.to_string().red().to_string());
-        } else {
-            s.push(item.character);
-        }
-    }
-    println!("{}, score: {}", s, score.to_string().bold().blue());
-}
+    let fuzzy_match = vscode_fuzzy_score_rs::fuzzy_match(&config.query, &config.target);
 
-#[derive(Debug, PartialEq)]
-struct Config {
-    pattern: String,
-    file: String,
-}
+    match fuzzy_match {
+        None => println!("'{}' does not match '{}'", config.query, config.target),
+        Some(fuzzy_match) => {
+            let mut colored_target = String::new();
+            let mut matches_it = fuzzy_match.positions().iter().peekable();
+            for (index, ch) in config.target.chars().enumerate() {
+                if matches_it.peek().is_some_and(|pos| **pos == index) {
+                    colored_target.push_str(&ch.to_string().blue().to_string());
+                    matches_it.next();
+                } else {
+                    colored_target.push(ch);
+                }
+            }
 
-impl Config {
-    fn new(args: impl Iterator<Item = String>) -> Config {
-        let matches = parse_args(args);
-
-        Config {
-            pattern: matches.get_one::<String>("pattern").unwrap().clone(),
-            file: matches.get_one::<String>("text").unwrap().clone(),
+            println!("{}, score: {}", colored_target, fuzzy_match.score());
         }
     }
 }
 
-fn parse_args(args: impl Iterator<Item = String>) -> ArgMatches {
-    Command::new(option_env!("CARGO_NAME").unwrap_or("fzgrep"))
-        .version(option_env!("CARGO_PKG_VERSION").unwrap_or("unknown"))
-        .author(option_env!("CARGO_EMAIL").unwrap_or("Andrii Semkiv <semkiv@gmail.com>"))
-        .arg(
-            Arg::new("pattern")
-                .value_name("PATTERN")
-                .help("Sets the pattern to match")
-                .required(true),
-        )
-        .arg(
-            Arg::new("text")
-                .value_name("TEXT")
-                .help("Sets the text to search in")
-                .required(true),
-        )
-        .get_matches_from(args)
+mod details {
+    use clap::{Arg, ArgMatches, Command};
+
+    #[derive(Debug, PartialEq)]
+    pub struct Config {
+        pub query: String,
+        pub target: String,
+    }
+
+    impl Config {
+        pub fn new(args: impl Iterator<Item = String>) -> Config {
+            let matches = parse_args(args);
+
+            Config {
+                query: matches.get_one::<String>("pattern").unwrap().clone(),
+                target: matches.get_one::<String>("text").unwrap().clone(),
+            }
+        }
+    }
+
+    fn parse_args(args: impl Iterator<Item = String>) -> ArgMatches {
+        Command::new(option_env!("CARGO_NAME").unwrap_or("fzgrep"))
+            .version(option_env!("CARGO_PKG_VERSION").unwrap_or("unknown"))
+            .author(option_env!("CARGO_EMAIL").unwrap_or("Andrii Semkiv <semkiv@gmail.com>"))
+            .arg(
+                Arg::new("pattern")
+                    .value_name("PATTERN")
+                    .help("Sets the pattern to match")
+                    .required(true),
+            )
+            .arg(
+                Arg::new("text")
+                    .value_name("TEXT")
+                    .help("Sets the text to search in")
+                    .required(true),
+            )
+            .get_matches_from(args)
+    }
 }
 
 #[cfg(test)]
@@ -74,8 +84,8 @@ mod test {
         assert_eq!(
             Config::new(args.into_iter()),
             Config {
-                pattern: String::from("pattern"),
-                file: String::from("file"),
+                query: String::from("pattern"),
+                target: String::from("file"),
             },
         )
     }
