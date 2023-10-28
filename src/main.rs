b014a10b9a8509073f19c7196ca623d6a8eeb4e1
Andrii Semkiv <semkiv@gmail.com>
2023-10-28 19:49:55 +0200

    Added support for grepping files.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    fzgrep::run();
+use env_logger;
+use std::env;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    env_logger::init();
+
+    fzgrep::run(env::args())
 }
