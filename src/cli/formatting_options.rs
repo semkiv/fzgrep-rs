acde610bb3d8052db42ff8de84245bc4aeb606c6
Andrii Semkiv <semkiv@gmail.com>
2023-11-06 22:57:37 +0100

    Added an option to control whether filenames should be printed. (#12)

--- src/cli/formatting_options.rs
+++ src/cli/formatting_options.rs
@@ -17,27 +17,48 @@
 #[derive(Clone, Copy, Debug, Default)]
 pub struct FormattingOptions {
     line_number: bool,
+    file_name: bool,
 }
 
-/// A simple getter that just returns the value of `--line-number` flag.
-///
-/// # Examples
-///
-/// ```
-/// let options = fzgrep::FormattingOptions::default();
-/// assert!(!options.line_number());
-/// ```
-///
-/// ```
-/// let options = fzgrep::FormattingOptionsBuilder::new()
-///     .line_number(true)
-///     .build();
-/// assert!(options.line_number());
-/// ```
-///
 impl FormattingOptions {
+    /// A simple getter that just returns the value of `--line-number` flag.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let options = fzgrep::FormattingOptions::default();
+    /// assert!(!options.line_number());
+    /// ```
+    ///
+    /// ```
+    /// let options = fzgrep::FormattingOptionsBuilder::new()
+    ///     .line_number(true)
+    ///     .build();
+    /// assert!(options.line_number());
+    /// ```
+    ///
     pub fn line_number(&self) -> bool {
         self.line_number
+    }
+
+    /// A simple getter for `--with-filename`/`--no-filename` flag.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let options = fzgrep::FormattingOptions::default();
+    /// assert!(!options.file_name());
+    /// ```
+    ///
+    /// ```
+    /// let options = fzgrep::FormattingOptionsBuilder::new()
+    ///     .file_name(true)
+    ///     .build();
+    /// assert!(options.file_name());
+    /// ```
+    ///
+    pub fn file_name(&self) -> bool {
+        self.file_name
     }
 }
 
@@ -47,13 +68,16 @@ impl FormattingOptions {
 /// ```
 /// let options = fzgrep::FormattingOptionsBuilder::new()
 ///     .line_number(true)
+///     .file_name(true)
 ///     .build();
 /// assert!(options.line_number());
+/// assert!(options.file_name())
 /// ```
 ///
 #[derive(Debug, Default, PartialEq)]
 pub struct FormattingOptionsBuilder {
     line_number: bool,
+    file_name: bool,
 }
 
 impl FormattingOptionsBuilder {
@@ -64,6 +88,7 @@ impl FormattingOptionsBuilder {
     /// let builder = fzgrep::FormattingOptionsBuilder::new();
     /// let options = builder.build();
     /// assert!(!options.line_number());
+    /// assert!(!options.file_name());
     /// ```
     ///
     pub fn new() -> Self {
@@ -85,18 +110,35 @@ impl FormattingOptionsBuilder {
         self
     }
 
+    /// Set `file_name` option to the provided value.
+    ///
+    /// # Examples
+    /// ```
+    /// let options = fzgrep::FormattingOptionsBuilder::new()
+    ///     .file_name(true)
+    ///     .build();
+    /// assert!(options.file_name());
+    /// ```
+    ///
+    pub fn file_name(mut self, file_name: bool) -> Self {
+        self.file_name = file_name;
+        self
+    }
+
     /// Consumes the builder object in exchange for a configured [`FormattingOptions`] object.
     ///
     /// # Examples
     /// ```
-    /// let builder = fzgrep::FormattingOptionsBuilder::new().line_number(true);
+    /// let builder = fzgrep::FormattingOptionsBuilder::new().line_number(true).file_name(true);
     /// let options = builder.build();
     /// assert!(options.line_number());
+    /// assert!(options.file_name());
     /// ```
     ///
     pub fn build(self) -> FormattingOptions {
         FormattingOptions {
             line_number: self.line_number,
+            file_name: self.file_name,
         }
     }
 }
@@ -108,27 +150,70 @@ mod test {
     #[test]
     fn builder_constructor() {
         let builder = FormattingOptionsBuilder::new();
-        assert_eq!(builder, FormattingOptionsBuilder { line_number: false });
+        assert_eq!(
+            builder,
+            FormattingOptionsBuilder {
+                line_number: false,
+                file_name: false
+            }
+        );
     }
 
     #[test]
     fn builder_line_number() {
         let builder = FormattingOptionsBuilder::new().line_number(false);
-        assert_eq!(builder, FormattingOptionsBuilder { line_number: false });
+        assert_eq!(
+            builder,
+            FormattingOptionsBuilder {
+                line_number: false,
+                file_name: false
+            }
+        );
         let builder = builder.line_number(true);
-        assert_eq!(builder, FormattingOptionsBuilder { line_number: true });
+        assert_eq!(
+            builder,
+            FormattingOptionsBuilder {
+                line_number: true,
+                file_name: false
+            }
+        );
+    }
+
+    #[test]
+    fn builder_file_name() {
+        let builder = FormattingOptionsBuilder::new().file_name(false);
+        assert_eq!(
+            builder,
+            FormattingOptionsBuilder {
+                line_number: false,
+                file_name: false
+            }
+        );
+        let builder = builder.file_name(true);
+        assert_eq!(
+            builder,
+            FormattingOptionsBuilder {
+                line_number: false,
+                file_name: true
+            }
+        );
     }
 
     #[test]
     fn builder_build() {
-        let options = FormattingOptionsBuilder::new().line_number(true).build();
+        let options = FormattingOptionsBuilder::new()
+            .line_number(true)
+            .file_name(true)
+            .build();
         assert!(options.line_number());
+        assert!(options.file_name());
     }
 
     #[test]
     fn options_default() {
         let default = FormattingOptions::default();
         assert!(!default.line_number());
+        assert!(!default.file_name());
     }
 
     #[test]
@@ -137,5 +222,13 @@ mod test {
         assert!(!line_number_off.line_number());
         let line_number_on = FormattingOptionsBuilder::new().line_number(true).build();
         assert!(line_number_on.line_number());
+    }
+
+    #[test]
+    fn options_file_name() {
+        let file_name_off = FormattingOptionsBuilder::new().file_name(false).build();
+        assert!(!file_name_off.file_name());
+        let file_name_on = FormattingOptionsBuilder::new().file_name(true).build();
+        assert!(file_name_on.file_name());
     }
 }
