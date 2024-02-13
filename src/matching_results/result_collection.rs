86913bf3305a342026252d044f7a3fc336b1ba9d
Andrii Semkiv <semkiv@gmail.com>
2024-02-13 22:57:07 +0100

    Turned `TopBracket` into a generic `struct`. Removed unused methods. Added unit tests.

--- src/matching_results/result_collection.rs
+++ src/matching_results/result_collection.rs
@@ -3,34 +3,16 @@ use crate::{MatchingResult, matching_results::top_bracket::TopBracket};
 
 pub trait ResultCollection {
     fn push(&mut self, result: MatchingResult);
-    fn merge(&mut self, other: Self);
-    fn is_full(&self) -> bool;
 }
 
 impl ResultCollection for Vec<MatchingResult> {
     fn push(&mut self, result: MatchingResult) {
         self.push(result);
     }
-
-    fn merge(&mut self, mut other: Self) {
-        self.append(&mut other);
-    }
-
-    fn is_full(&self) -> bool {
-        false
-    }
 }
 
-impl ResultCollection for TopBracket {
+impl ResultCollection for TopBracket<MatchingResult> {
     fn push(&mut self, result: MatchingResult) {
         self.push(result);
-    }
-
-    fn merge(&mut self, other: Self) {
-        self.merge(other);
-    }
-
-    fn is_full(&self) -> bool {
-        self.is_full()
     }
 }
--- src/matching_results/top_bracket.rs
+++ src/matching_results/top_bracket.rs
@@ -1,11 +1,9 @@
-use crate::MatchingResult;
-
-pub(crate) struct TopBracket {
+pub(crate) struct TopBracket<T> {
     capacity: usize,
-    data: Vec<MatchingResult>,
+    data: Vec<T>,
 }
 
-impl TopBracket {
+impl<T> TopBracket<T> {
     pub(crate) fn new(capacity: usize) -> Self {
         Self {
             capacity,
@@ -13,31 +11,94 @@ impl TopBracket {
         }
     }
 
-    pub(crate) fn push(&mut self, matching_result: MatchingResult) {
+    pub(crate) fn into_vec(self) -> Vec<T> {
+        self.data
+    }
+}
+
+impl<T: Ord> TopBracket<T> {
+    pub(crate) fn push(&mut self, item: T) -> bool {
         if self.data.len() == self.capacity {
-            if matching_result <= *self.data.last().unwrap() {
-                return;
+            if item <= *self.data.last().unwrap() {
+                return false;
             }
 
             self.data.pop();
         }
 
-        self.data.push(matching_result);
+        self.data.push(item);
         self.data.sort_by(|a, b| b.cmp(a));
+        true
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn constructor() {
+        let capacity = 4;
+        let container = TopBracket::<i32>::new(capacity);
+        assert_eq!(container.capacity, capacity);
+        assert_eq!(container.data.len(), 0);
+        assert_eq!(container.data.capacity(), capacity);
     }
 
-    pub(crate) fn merge(&mut self, other: Self) {
-        // TODO: this is a very suboptimal algorithm, it must be improved.
-        for el in other.data {
-            self.push(el);
-        }
+    #[test]
+    fn push() {
+        let capacity = 4;
+        let mut container = TopBracket::new(capacity);
+        assert_eq!(container.capacity, capacity);
+        assert_eq!(container.data.len(), 0);
+        assert_eq!(container.data.capacity(), capacity);
+
+        assert!(container.push(1));
+        assert_eq!(container.data.len(), 1);
+        assert_eq!(container.data, [1]);
+
+        assert!(container.push(2));
+        assert_eq!(container.data.len(), 2);
+        assert_eq!(container.data, [2, 1]);
+
+        assert!(container.push(0));
+        assert_eq!(container.data.len(), 3);
+        assert_eq!(container.data, [2, 1, 0]);
+
+        assert!(container.push(1));
+        assert_eq!(container.data.len(), 4);
+        assert_eq!(container.data, [2, 1, 1, 0]);
+
+        assert!(!container.push(-1));
+        assert_eq!(container.data.len(), 4);
+        assert_eq!(container.data, [2, 1, 1, 0]);
+
+        assert!(!container.push(0));
+        assert_eq!(container.data.len(), 4);
+        assert_eq!(container.data, [2, 1, 1, 0]);
+
+        assert!(container.push(2));
+        assert_eq!(container.data.len(), 4);
+        assert_eq!(container.data, [2, 2, 1, 1]);
     }
 
-    pub(crate) fn into_vec(self) -> Vec<MatchingResult> {
-        self.data
-    }
+    #[test]
+    fn into_vec() {
+        let capacity = 4;
+        let container = TopBracket::<i32>::new(capacity);
+        assert_eq!(container.into_vec(), []);
 
-    pub(crate) fn is_full(&self) -> bool {
-        self.data.len() == self.capacity
+        let mut container = TopBracket::new(capacity);
+        container.push(1);
+        container.push(2);
+        assert_eq!(container.into_vec(), [2, 1]);
+
+        let mut container = TopBracket::new(capacity);
+        container.push(1);
+        container.push(2);
+        container.push(0);
+        container.push(1);
+        container.push(2);
+        assert_eq!(container.into_vec(), [2, 2, 1, 1]);
     }
 }
