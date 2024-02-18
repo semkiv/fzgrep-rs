96a824c7eb38436a6bce996cf88e6252d8f7943c
Andrii Semkiv <semkiv@gmail.com>
2024-02-18 16:25:28 +0100

    Added a small doc and a couple of unit-tests.

--- src/matching_results/result_collection.rs
+++ src/matching_results/result_collection.rs
@@ -1,7 +1,10 @@
-use crate::{MatchingResult, matching_results::top_bracket::TopBracket};
+use crate::{matching_results::top_bracket::TopBracket, MatchingResult};
 
-
-pub trait ResultCollection {
+/// A trait that generalizes interface between possible results containers
+/// As it currently stands, only one method is required to be provided -
+/// the one that adds an item into the container.
+///
+pub(crate) trait ResultCollection {
     fn push(&mut self, result: MatchingResult);
 }
 
@@ -14,5 +17,80 @@ impl ResultCollection for Vec<MatchingResult> {
 impl ResultCollection for TopBracket<MatchingResult> {
     fn push(&mut self, result: MatchingResult) {
         self.push(result);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::matching_results::result::Context;
+
+    fn do_push<T: ResultCollection>(tested: &mut T, item: MatchingResult) -> &T {
+        tested.push(item);
+        tested
+    }
+
+    #[test]
+    fn push_vec() {
+        let mut v = vec![MatchingResult {
+            matching_line: String::from("test_vec"),
+            fuzzy_match: vscode_fuzzy_score_rs::fuzzy_match("test_vec", "test_vec").unwrap(),
+            file_name: None,
+            line_number: None,
+            context: Context {
+                before: Vec::new(),
+                after: Vec::new(),
+            },
+        }];
+        let item = MatchingResult {
+            matching_line: String::from("test"),
+            fuzzy_match: vscode_fuzzy_score_rs::fuzzy_match("test", "test").unwrap(),
+            file_name: None,
+            line_number: None,
+            context: Context {
+                before: Vec::new(),
+                after: Vec::new(),
+            },
+        };
+        let expected = {
+            let mut v = v.clone();
+            v.push(item.clone());
+            v
+        };
+
+        assert_eq!(*do_push(&mut v, item), expected);
+    }
+
+    #[test]
+    fn push_top_bracket() {
+        let mut tb = TopBracket::new(1);
+        tb.push(MatchingResult {
+            matching_line: String::from("test_top_bracket"),
+            fuzzy_match: vscode_fuzzy_score_rs::fuzzy_match("test_top_bracket", "test_top_bracket")
+                .unwrap(),
+            file_name: None,
+            line_number: None,
+            context: Context {
+                before: Vec::new(),
+                after: Vec::new(),
+            },
+        });
+        let item = MatchingResult {
+            matching_line: String::from("test"),
+            fuzzy_match: vscode_fuzzy_score_rs::fuzzy_match("test", "test").unwrap(),
+            file_name: None,
+            line_number: None,
+            context: Context {
+                before: Vec::new(),
+                after: Vec::new(),
+            },
+        };
+        let expected = {
+            let mut tb = tb.clone();
+            tb.push(item.clone());
+            tb
+        };
+
+        assert_eq!(*do_push(&mut tb, item.clone()), expected);
     }
 }
